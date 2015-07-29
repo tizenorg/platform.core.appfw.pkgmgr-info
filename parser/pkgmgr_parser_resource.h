3ed6a95da5c74062f6d066aa19c02c7b4310a5b2
Jiwoong Im <jiwoong.im@samsung.com>
2015-07-29 15:55:19 +0900

    modify resource manager source.
    - remove db saving/loading while using resource manager
    
    Change-Id: Id8d00ddcf1f731e236e12be9bc37cb8b69f29861
    Signed-off-by: Jiwoong Im <jiwoong.im@samsung.com>

--- parser/pkgmgr_parser_resource.h
+++ parser/pkgmgr_parser_resource.h
@@ -24,18 +24,17 @@ extern "C" {
 #endif
 
 /**
- * @fn int *pkgmgr_resource_parser_open(const char *fname, const char *package, resource_data_t **data)
+ * @fn int *pkgmgr_resource_parser_open(const char *fname, resource_data_t **data)
  * @brief	This API initialize parses res.xml which identified by fname and package and put it into data.
  *
  * @par		This API is for package-manager installer backends.
  * @par Sync (or) Async : Synchronous API
  *
  * @param[in]	fname		pointer to xml filename
- * @param[in]	pacakage	pointer to packageID
  * @param[out]data		pointer of	pointer to resource_data type structure.
  * @return	0 on succeed and -1 on failure, -2 on invalid parameter
  */
-API int pkgmgr_resource_parser_open(const char *fname, const char *package, resource_data_t **data);
+API int pkgmgr_resource_parser_open(const char *fname, resource_data_t **data);
 
 /**
  * @fn int pkgmgr_resource_parser_close(resource_data_t data)
@@ -50,31 +49,6 @@ API int pkgmgr_resource_parser_open(const char *fname, const char *package, reso
 API int pkgmgr_resource_parser_close(resource_data_t *data);
 
 /**
- * @fn int pkgmgr_resource_parser_insert_into_db(resource_data_t *data)
- * @brief	This API will put given data into db
- *
- * @par		This API is for package-manager installer backends.
- * @par Sync (or) Async : Synchronous API
- *
- * @param[in]	data	structure to be inserted into db
- * @return	0 on succeed and -1 on failure, -2 on invalid parameter
- */
-API int pkgmgr_resource_parser_insert_into_db(resource_data_t *data);
-
-/**
- * @fn int pkgmgr_resource_parser_open_from_db(const char *package, resource_data_t **data)
- * @brief	This API will get resource data of specific package from db
- *
- * @par		This API is for applications.
- * @par Sync (or) Async : Synchronous API
- *
- * @param[in]	package	packageID
- * @param[out]data	resource_data type structure. it will filled with resource data
- * @return	0 on succeed and -1 on failure, -2 on invalid parameter
- */
-API int pkgmgr_resource_parser_open_from_db(const char *package, resource_data_t **data);
-
-/**
  * @fn int pkgmgr_resource_parser_check_xml_validation(const char *xmlfile)
  * @brief	This API will validates given resource manifest file
  *
@@ -85,18 +59,6 @@ API int pkgmgr_resource_parser_open_from_db(const char *package, resource_data_t
  * @return	0 on succeed and -1 on failure, -2 on invalid parameter
  */
 API int pkgmgr_resource_parser_check_xml_validation(const char *xmlfile);
-
-/**
- * @fn int pkgmgr_resource_parser_delete_from_db(const char *package)
- * @brief	This API will remove resource data from package_resource_info, package_resource_data
- *
- * @par		This API is for package-manager installer backends.
- * @par Sync (or) Async : Synchronous API
- *
- * @param[in]	package	packageID
- * @return	0 on succeed and -1 on failure, -2 on invalid parameter
- */
-API int pkgmgr_resource_parser_delete_from_db(const char *package);
 
 #ifdef __cplusplus
 }
