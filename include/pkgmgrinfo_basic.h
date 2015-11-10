5390e83eb63b543bc226bf0b1b8c5584b70741a7
jongmyeongko <jongmyeong.ko@samsung.com>
2015-11-10 13:36:11 +0900

    merge tizen 2.3/2.4 to xsd and related structure type for parsing manifest
    Change-Id: I28fd3cbdfa625081364528684ef26dea25a74a62
    Signed-off-by: jongmyeongko <jongmyeong.ko@samsung.com>

--- include/pkgmgrinfo_basic.h
+++ include/pkgmgrinfo_basic.h
@@ -92,81 +92,91 @@ typedef struct datacontrol_x {
 } datacontrol_x;
 
 typedef struct application_x {
-	const char *appid;
-	const char *component;
-	const char *exec;
-	const char *nodisplay;
-	const char *type;
-	const char *onboot;
-	const char *multiple;
-	const char *autorestart;
-	const char *taskmanage;
-	const char *enabled;
-	const char *categories;
-	const char *extraid;
-	const char *hwacceleration;
-	const char *screenreader;
-	const char *mainapp;
-	const char *recentimage;
-	const char *launchcondition;
-	const char *indicatordisplay;
-	const char *portraitimg;
-	const char *landscapeimg;
-	const char *guestmode_visibility;
-	const char *permission_type;
-	const char *preload;
-	const char *submode;
-	const char *submode_mainid;
-	const char *launch_mode;
-	const char *ui_gadget;
-	const char *support_disable;
-	const char *component_type;
-	const char *package;
-	GList *label;
-	GList *icon;
-	GList *image;
-	GList *appcontrol;
-	GList *category;
-	GList *metadata;
-	GList *permission;
+	const char *appid;	/*attr*/
+	const char *exec;	/*attr*/
+	const char *nodisplay;	/*attr, default: "false"*/
+	const char *multiple;	/*attr, default: "false"*/
+	const char *taskmanage;	/*attr, default: "true"*/
+	const char *enabled;	/*attr, default: "true"*/
+	const char *type;	/*attr*/
+	const char *categories;	/*attr*/
+	const char *extraid;	/*attr*/
+	const char *hwacceleration;	/*attr, default: "default"*/
+	const char *screenreader;	/*attr, default: "use-system-setting"*/
+	const char *mainapp;	/*attr, default: "false"*/
+	const char *package;	/*set from package_x*/
+	const char *recentimage;	/*attr, default: "false"*/
+	const char *launchcondition;	/*attr, default: "false"*/
+	const char *indicatordisplay;	/*attr, default: "true"*/
+	const char *portraitimg;	/*attr*/
+	const char *landscapeimg;	/*attr*/
+	const char *effectimage_type;	/*attr, default: "image"*/
+	const char *guestmode_visibility;	/*attr, default: "true"*/
+	const char *component;	/*no xml part*/
+	const char *permission_type;	/*attr, default: "normal"*/
+	const char *component_type;	/*attr, default: "uiapp"*/
+	const char *preload;	/*no xml part*/
+	const char *submode;	/*attr, default: "false"*/
+	const char *submode_mainid;	/*attr, default: "false"*/
+	const char *process_pool;	/*attr, default: "false"*/
+	const char *autorestart;	/*attr, default: "false"*/
+	const char *onboot;	/*attr, default: "false"*/
+	const char *multi_instance;	/*attr, default: "false"*/
+	const char *multi_instance_mainid;	/*attr*/
+	const char *support_disable;	/*set from package_x*/
+	const char *ui_gadget;	/*attr, default: "false"*/
+	const char *launch_mode;	/*attr, default: "single"*/
+	const char *ambient_support;	/*attr, default: "false"*/
+	GList *label;	/*element*/
+	GList *icon;	/*element*/
+	GList *image;	/*element*/
+	GList *category; /*element*/
+	GList *metadata;	/*element*/
+	GList *permission;	/*element*/
 	GList *launchconditions;
 	GList *notification;
 	GList *datashare;
-	GList *datacontrol;
+	GList *datacontrol; /*element*/
+	GList *background_category; /*element*/
+	GList *appcontrol; /*element*/
 } application_x;
 
 typedef struct package_x {
 	const char *for_all_users;		/**< Flag that indicates if the package is available for everyone or for current user only*/
-	const char *package;		/**< package name*/
-	const char *version;		/**< package version*/
-	const char *installlocation;		/**< package install location*/
-	const char *ns;		/**<name space*/
-	const char *removable;		/**< package removable flag*/
-	const char *preload;		/**< package preload flag*/
-	const char *readonly;		/**< package readonly flag*/
-	const char *update;			/**< package update flag*/
-	const char *appsetting;		/**< package app setting flag*/
-	const char *system;		/**< package system flag*/
-	const char *type;		/**< package type*/
-	const char *package_size;		/**< package size for external installation*/
-	const char *installed_time;		/**< installed time after finishing of installation*/
-	const char *installed_storage;		/**< package currently installed storage*/
-	const char *storeclient_id;		/**< id of store client for installed package*/
-	const char *mainapp_id;		/**< app id of main application*/
-	const char *package_url;		/**< app id of main application*/
-	const char *root_path;		/**< package root path*/
-	const char *csc_path;		/**< package csc path*/
-	const char *nodisplay_setting;		/**< package no display setting menu*/
-	const char *api_version;		/**< minimum version of API package using*/
-	const char *support_disable;		/**< package support disable flag*/
-	GList *icon;		/**< package icon*/
-	GList *label;		/**< package label*/
-	GList *author;		/**< package author*/
-	GList *description;		/**< package description*/
-	GList *license;		/**< package license*/
-	GList *privileges;	/**< package privileges*/
-	GList *application;		/**< package's application*/
-	GList *compatibility;		/**< package compatibility*/
+	const char *package;		/**< package name, attr*/
+	const char *version;		/**< package version, attr*/
+	const char *installlocation;		/**< package install location, attr, default: "internal-only"*/
+	const char *ns;		/**<name space, attr*/
+	const char *removable;		/**< package removable flag, no xml part*/
+	const char *preload;		/**< package preload flag, no xml part*/
+	const char *readonly;		/**< package readonly flag, no xml part*/
+	const char *update;			/**< package update flag, no xml part*/
+	const char *appsetting;		/**< package app setting flag, attr, default: "false"*/
+	const char *system;		/**< package system flag, no xml part*/
+	const char *type;		/**< package type, attr, default: "rpm"*/
+	const char *package_size;		/**< package size for external installation, attr*/
+	const char *installed_time;		/**< installed time after finishing of installation, no xml part*/
+	const char *installed_storage;		/**< package currently installed storage, no xml part*/
+	const char *storeclient_id;		/**< id of store client for installed package, attr*/
+	const char *mainapp_id;		/**< app id of main application, no xml part*/
+	const char *package_url;		/**< app id of main application, attr*/
+	const char *root_path;		/**< package root path, attr*/
+	const char *csc_path;		/**< package csc path, attr*/
+	const char *nodisplay_setting;		/**< package no display setting menu, attr, default: "false"*/
+	const char *support_disable;		/**< package support disable flag, attr, default: "false"*/
+	const char *api_version;		/**< minimum version of API package using, attr, default: patch_version trimmed version from tizen_full_version*/
+	const char *tep_name; /*no xml part*/
+	const char *backend_installer;		/**< package backend installer, attr*/
+	const char *custom_smack_label;		/**< package custom smack label, attr*/
+	GList *icon;		/**< package icon, element*/
+	GList *label;		/**< package label, element*/
+	GList *author;		/**< package author, element*/
+	GList *description;		/**< package description, element*/
+	GList *license;		/**< package license, no xml part*/
+	GList *privileges;	/**< package privileges, element*/
+	GList *application;		/**< package's application, element*/
+	GList *compatibility;		/**< package compatibility, element*/
+	GList *deviceprofile;		/**< package device profile, element*/
 } package_x;
 
 typedef struct package_x manifest_x;
