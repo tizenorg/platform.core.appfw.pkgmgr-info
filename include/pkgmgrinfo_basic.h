e00f502be61895acef1830da5de78523ed9daa16
Hwankyu Jhun <h.jhun@samsung.com>
2016-03-10 07:44:22 +0900

    Remove compile warning messages
    Change-Id: Ia12c7317158f5df7b8339842568b3f016a988b56
    Signed-off-by: Hwankyu Jhun <h.jhun@samsung.com>

--- include/pkgmgrinfo_basic.h
+++ include/pkgmgrinfo_basic.h
@@ -4,32 +4,32 @@
 #include <glib.h>
 
 typedef struct metadata_x {
-	const char *key;
-	const char *value;
+	char *key;
+	char *value;
 } metadata_x;
 
 typedef struct permission_x {
-	const char *type;
-	const char *value;
+	char *type;
+	char *value;
 } permission_x;
 
 typedef struct icon_x {
-	const char *text;
-	const char *lang;
-	const char *section;
-	const char *size;
-	const char *resolution;
-	const char *dpi;
+	char *text;
+	char *lang;
+	char *section;
+	char *size;
+	char *resolution;
+	char *dpi;
 } icon_x;
 
 typedef struct image_x {
-	const char *text;
-	const char *lang;
-	const char *section;
+	char *text;
+	char *lang;
+	char *section;
 } image_x;
 
 typedef struct define_x {
-	const char *path;
+	char *path;
 	GList *allowed;
 	GList *request;
 } define_x;
@@ -40,107 +40,107 @@ typedef struct datashare_x {
 } datashare_x;
 
 typedef struct description_x {
-	const char *name;
-	const char *text;
-	const char *lang;
+	char *name;
+	char *text;
+	char *lang;
 } description_x;
 
 typedef struct label_x {
-	const char *name;
-	const char *text;
-	const char *lang;
+	char *name;
+	char *text;
+	char *lang;
 } label_x;
 
 typedef struct author_x {
-	const char *email;
-	const char *href;
-	const char *text;
-	const char *lang;
+	char *email;
+	char *href;
+	char *text;
+	char *lang;
 } author_x;
 
 typedef struct license_x {
-	const char *text;
-	const char *lang;
+	char *text;
+	char *lang;
 } license_x;
 
 typedef struct condition_x {
-	const char *name;
-	const char *text;
+	char *name;
+	char *text;
 } condition_x;
 
 typedef struct notification_x {
-	const char *name;
-	const char *text;
+	char *name;
+	char *text;
 } notification_x;
 
 typedef struct appcontrol_x {
-	const char *operation;
-	const char *uri;
-	const char *mime;
+	char *operation;
+	char *uri;
+	char *mime;
 } appcontrol_x;
 
 typedef struct compatibility_x {
-	const char *name;
-	const char *text;
+	char *name;
+	char *text;
 } compatibility_x;
 
 typedef struct datacontrol_x {
-	const char *providerid;
-	const char *access;
-	const char *type;
+	char *providerid;
+	char *access;
+	char *type;
 } datacontrol_x;
 
 typedef struct splashscreen_x {
-	const char *src;
-	const char *type;
-	const char *dpi;
-	const char *orientation;
-	const char *indicatordisplay;
-	const char *operation;
+	char *src;
+	char *type;
+	char *dpi;
+	char *orientation;
+	char *indicatordisplay;
+	char *operation;
 } splashscreen_x;
 
 typedef struct application_x {
-	const char *appid;	/*attr*/
-	const char *exec;	/*attr*/
-	const char *nodisplay;	/*attr, default: "false"*/
-	const char *multiple;	/*attr, default: "false"*/
-	const char *taskmanage;	/*attr, default: "true"*/
-	const char *enabled;	/*attr, default: "true"*/
-	const char *type;	/*attr*/
-	const char *categories;	/*attr*/
-	const char *extraid;	/*attr*/
-	const char *hwacceleration;	/*attr, default: "default"*/
-	const char *screenreader;	/*attr, default: "use-system-setting"*/
-	const char *mainapp;	/*attr, default: "false"*/
-	const char *package;	/*set from package_x*/
-	const char *recentimage;	/*attr, default: "false"*/
-	const char *launchcondition;	/*attr, default: "false"*/
-	const char *indicatordisplay;	/*attr, default: "true"*/
-	const char *portraitimg;	/*attr*/
-	const char *landscapeimg;	/*attr*/
-	const char *effectimage_type;	/*attr, default: "image"*/
-	const char *guestmode_visibility;	/*attr, default: "true"*/
-	const char *component;	/*no xml part*/
-	const char *permission_type;	/*attr, default: "normal"*/
-	const char *component_type;	/*attr, default: "uiapp"*/
-	const char *preload;	/*no xml part*/
-	const char *submode;	/*attr, default: "false"*/
-	const char *submode_mainid;	/*attr, default: "false"*/
-	const char *process_pool;	/*attr, default: "false"*/
-	const char *installed_storage;
-	const char *autorestart;	/*attr, default: "false"*/
-	const char *onboot;	/*attr, default: "false"*/
-	const char *support_disable;	/*set from package_x*/
-	const char *ui_gadget;	/*attr, default: "false"*/
-	const char *launch_mode;	/*attr, default: "single"*/
-	const char *ambient_support;	/*attr, default: "false"*/
-	const char *alias_appid;	/*attr*/
-	const char *effective_appid;	/*attr*/
-	const char *package_type;	/*set from package_x*/
-	const char *tep_name;	/*set from package_x*/
-	const char *root_path;	/*set from package_x*/
-	const char *api_version;	/*set from package_x*/
-	const char *for_all_users; /**< Flag that indicates if the package is available for everyone or for current user only, no xml part*/
+	char *appid;	/*attr*/
+	char *exec;	/*attr*/
+	char *nodisplay;	/*attr, default: "false"*/
+	char *multiple;	/*attr, default: "false"*/
+	char *taskmanage;	/*attr, default: "true"*/
+	char *enabled;	/*attr, default: "true"*/
+	char *type;	/*attr*/
+	char *categories;	/*attr*/
+	char *extraid;	/*attr*/
+	char *hwacceleration;	/*attr, default: "default"*/
+	char *screenreader;	/*attr, default: "use-system-setting"*/
+	char *mainapp;	/*attr, default: "false"*/
+	char *package;	/*set from package_x*/
+	char *recentimage;	/*attr, default: "false"*/
+	char *launchcondition;	/*attr, default: "false"*/
+	char *indicatordisplay;	/*attr, default: "true"*/
+	char *portraitimg;	/*attr*/
+	char *landscapeimg;	/*attr*/
+	char *effectimage_type;	/*attr, default: "image"*/
+	char *guestmode_visibility;	/*attr, default: "true"*/
+	char *component;	/*no xml part*/
+	char *permission_type;	/*attr, default: "normal"*/
+	char *component_type;	/*attr, default: "uiapp"*/
+	char *preload;	/*no xml part*/
+	char *submode;	/*attr, default: "false"*/
+	char *submode_mainid;	/*attr, default: "false"*/
+	char *process_pool;	/*attr, default: "false"*/
+	char *installed_storage;
+	char *autorestart;	/*attr, default: "false"*/
+	char *onboot;	/*attr, default: "false"*/
+	char *support_disable;	/*set from package_x*/
+	char *ui_gadget;	/*attr, default: "false"*/
+	char *launch_mode;	/*attr, default: "single"*/
+	char *ambient_support;	/*attr, default: "false"*/
+	char *alias_appid;	/*attr*/
+	char *effective_appid;	/*attr*/
+	char *package_type;	/*set from package_x*/
+	char *tep_name;	/*set from package_x*/
+	char *root_path;	/*set from package_x*/
+	char *api_version;	/*set from package_x*/
+	char *for_all_users; /**< Flag that indicates if the package is available for everyone or for current user only, no xml part*/
 	GList *label;	/*element*/
 	GList *icon;	/*element*/
 	GList *image;	/*element*/
@@ -157,31 +157,31 @@ typedef struct application_x {
 } application_x;
 
 typedef struct package_x {
-	const char *for_all_users;		/**< Flag that indicates if the package is available for everyone or for current user only, no xml part*/
-	const char *package;		/**< package name, attr*/
-	const char *version;		/**< package version, attr*/
-	const char *installlocation;		/**< package install location, attr, default: "internal-only"*/
-	const char *ns;		/**<name space, attr*/
-	const char *removable;		/**< package removable flag, no xml part*/
-	const char *preload;		/**< package preload flag, no xml part*/
-	const char *readonly;		/**< package readonly flag, no xml part*/
-	const char *update;			/**< package update flag, no xml part*/
-	const char *appsetting;		/**< package app setting flag, attr, default: "false"*/
-	const char *system;		/**< package system flag, no xml part*/
-	const char *type;		/**< package type, attr*/
-	const char *package_size;		/**< package size for external installation, attr*/
-	const char *installed_time;		/**< installed time after finishing of installation, no xml part*/
-	const char *installed_storage;		/**< package currently installed storage, no xml part*/
-	const char *storeclient_id;		/**< id of store client for installed package, attr*/
-	const char *mainapp_id;		/**< app id of main application, no xml part*/
-	const char *package_url;		/**< app id of main application, attr*/
-	const char *root_path;		/**< package root path, attr*/
-	const char *csc_path;		/**< package csc path, attr*/
-	const char *nodisplay_setting;		/**< package no display setting menu, attr, default: "false"*/
-	const char *support_disable;		/**< package support disable flag, attr, default: "false"*/
-	const char *api_version;		/**< minimum version of API package using, attr, default: patch_version trimmed version from tizen_full_version*/
-	const char *tep_name;	/*no xml part*/
-	const char *backend_installer;		/**< package backend installer, attr*/
+	char *for_all_users;		/**< Flag that indicates if the package is available for everyone or for current user only, no xml part*/
+	char *package;		/**< package name, attr*/
+	char *version;		/**< package version, attr*/
+	char *installlocation;		/**< package install location, attr, default: "internal-only"*/
+	char *ns;		/**<name space, attr*/
+	char *removable;		/**< package removable flag, no xml part*/
+	char *preload;		/**< package preload flag, no xml part*/
+	char *readonly;		/**< package readonly flag, no xml part*/
+	char *update;			/**< package update flag, no xml part*/
+	char *appsetting;		/**< package app setting flag, attr, default: "false"*/
+	char *system;		/**< package system flag, no xml part*/
+	char *type;		/**< package type, attr*/
+	char *package_size;		/**< package size for external installation, attr*/
+	char *installed_time;		/**< installed time after finishing of installation, no xml part*/
+	char *installed_storage;		/**< package currently installed storage, no xml part*/
+	char *storeclient_id;		/**< id of store client for installed package, attr*/
+	char *mainapp_id;		/**< app id of main application, no xml part*/
+	char *package_url;		/**< app id of main application, attr*/
+	char *root_path;		/**< package root path, attr*/
+	char *csc_path;		/**< package csc path, attr*/
+	char *nodisplay_setting;		/**< package no display setting menu, attr, default: "false"*/
+	char *support_disable;		/**< package support disable flag, attr, default: "false"*/
+	char *api_version;		/**< minimum version of API package using, attr, default: patch_version trimmed version from tizen_full_version*/
+	char *tep_name;	/*no xml part*/
+	char *backend_installer;		/**< package backend installer, attr*/
 	GList *icon;		/**< package icon, element*/
 	GList *label;		/**< package label, element*/
 	GList *author;		/**< package author, element*/
