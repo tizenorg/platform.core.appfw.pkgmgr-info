285896879661bdf8dfa17d52ff3e76277f0d52eb
Junghyun Yeon <jungh.yeon@samsung.com>
2016-03-15 17:49:49 +0900

    change functions for thread-safe
    Change-Id: I2c88ceaa0d80bcbfd0f7fb867f0516dc00f31e25
    Signed-off-by: Junghyun Yeon <jungh.yeon@samsung.com>

--- parser/pkgmgr_parser_db.c
+++ parser/pkgmgr_parser_db.c
@@ -2379,10 +2379,11 @@ API int pkgmgr_parser_initialize_db(uid_t uid)
 static int __parserdb_change_perm(const char *db_file, uid_t uid)
 {
 	char buf[BUFSIZE];
+	char pwuid_buf[1024] = {0, };
 	char journal_file[BUFSIZE];
 	char *files[3];
 	int ret, i;
-	struct passwd *userinfo = NULL;
+	struct passwd userinfo, *result = NULL;
 	files[0] = (char *)db_file;
 	files[1] = journal_file;
 	files[2] = NULL;
@@ -2396,20 +2397,20 @@ static int __parserdb_change_perm(const char *db_file, uid_t uid)
 	snprintf(journal_file, sizeof(journal_file), "%s%s", db_file, "-journal");
 	if (uid == OWNER_ROOT)
 		uid = GLOBAL_USER;
-	userinfo = getpwuid(uid);
-	if (!userinfo) {
+	ret = getpwuid_r(uid, &userinfo, &pwuid_buf, sizeof(pwuid_buf), &result);
+	if (ret != 0 || result == NULL) {
 		_LOGE("FAIL: user %d doesn't exist", uid);
 		return -1;
 	}
 	snprintf(journal_file, sizeof(journal_file), "%s%s", db_file, "-journal");
 
 	for (i = 0; files[i]; i++) {
-		ret = chown(files[i], uid, userinfo->pw_gid);
+		ret = chown(files[i], uid, userinfo.pw_gid);
 		if (ret == -1) {
 			if (strerror_r(errno, buf, sizeof(buf)))
 				strncpy(buf, "", BUFSIZE - 1);
 			_LOGD("FAIL : chown %s %d.%d : %s", files[i], uid,
-					userinfo->pw_gid, buf);
+					userinfo.pw_gid, buf);
 			return -1;
 		}
 
