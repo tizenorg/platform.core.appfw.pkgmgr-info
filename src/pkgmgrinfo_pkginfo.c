d430b48cf9e50a0888483129325e2bb07c7ac4c6
Sangyoon Jang <s89.jang@samsung.com>
2016-07-05 20:36:52 +0900

    Fix returning empty value on getter
    Change-Id: I8e23937b0f455a2a140ee84930739670241f910c
    Signed-off-by: Sangyoon Jang <s89.jang@samsung.com>

--- src/pkgmgrinfo_pkginfo.c
+++ src/pkgmgrinfo_pkginfo.c
@@ -1181,13 +1181,11 @@ API int pkgmgrinfo_pkginfo_get_icon(pkgmgrinfo_pkginfo_h handle, char **icon)
 	locale = info->locale;
 	retvm_if(locale == NULL, PMINFO_R_ERROR, "manifest locale is NULL");
 
-	if (info->pkg_info == NULL)
+	if (info->pkg_info == NULL || info->pkg_info->icon == NULL)
 		return PMINFO_R_ERROR;
 
 	for (tmp = info->pkg_info->icon; tmp; tmp = tmp->next) {
 		ptr = (icon_x *)tmp->data;
-		if (ptr && ptr->text == NULL)
-			ptr->text = strdup("");
 		if (ptr == NULL || ptr->text == NULL || ptr->lang == NULL ||
 				!strcasecmp(ptr->text, "(null)") ||
 				strcmp(ptr->lang, locale))
@@ -1199,8 +1197,6 @@ API int pkgmgrinfo_pkginfo_get_icon(pkgmgrinfo_pkginfo_h handle, char **icon)
 	locale = DEFAULT_LOCALE;
 	for (tmp = info->pkg_info->icon; tmp; tmp = tmp->next) {
 		ptr = (icon_x *)tmp->data;
-		if (ptr && ptr->text == NULL)
-			ptr->text = strdup("");
 		if (ptr == NULL || ptr->text == NULL || ptr->lang == NULL ||
 				strcmp(ptr->lang, locale))
 			continue;
@@ -1208,7 +1204,9 @@ API int pkgmgrinfo_pkginfo_get_icon(pkgmgrinfo_pkginfo_h handle, char **icon)
 		return PMINFO_R_OK;
 	}
 
-	return PMINFO_R_ERROR;
+	*icon = "";
+
+	return PMINFO_R_OK;
 }
 
 API int pkgmgrinfo_pkginfo_get_label(pkgmgrinfo_pkginfo_h handle, char **label)
@@ -1224,10 +1222,11 @@ API int pkgmgrinfo_pkginfo_get_label(pkgmgrinfo_pkginfo_h handle, char **label)
 	locale = info->locale;
 	retvm_if(locale == NULL, PMINFO_R_ERROR, "manifest locale is NULL");
 
+	if (info->pkg_info == NULL || info->pkg_info->label == NULL)
+		return PMINFO_R_ERROR;
+
 	for (tmp = info->pkg_info->label; tmp != NULL; tmp = tmp->next) {
 		ptr = (label_x *)tmp->data;
-		if (ptr && ptr->text == NULL)
-			ptr->text = strdup("");
 		if (ptr == NULL || ptr->text == NULL || ptr->lang == NULL ||
 				strcmp(ptr->lang, locale))
 			continue;
@@ -1238,8 +1237,6 @@ API int pkgmgrinfo_pkginfo_get_label(pkgmgrinfo_pkginfo_h handle, char **label)
 	locale = DEFAULT_LOCALE;
 	for (tmp = info->pkg_info->label; tmp != NULL; tmp = tmp->next) {
 		ptr = (label_x *)tmp->data;
-		if (ptr && ptr->text == NULL)
-			ptr->text = strdup("");
 		if (ptr == NULL || ptr->text == NULL || ptr->lang == NULL ||
 				strcmp(ptr->lang, locale))
 			continue;
@@ -1247,7 +1244,9 @@ API int pkgmgrinfo_pkginfo_get_label(pkgmgrinfo_pkginfo_h handle, char **label)
 		return PMINFO_R_OK;
 	}
 
-	return PMINFO_R_ERROR;
+	*label = "";
+
+	return PMINFO_R_OK;
 }
 
 API int pkgmgrinfo_pkginfo_get_description(pkgmgrinfo_pkginfo_h handle, char **description)
@@ -1263,10 +1262,11 @@ API int pkgmgrinfo_pkginfo_get_description(pkgmgrinfo_pkginfo_h handle, char **d
 	locale = info->locale;
 	retvm_if(locale == NULL, PMINFO_R_ERROR, "manifest locale is NULL");
 
+	if (info->pkg_info == NULL || info->pkg_info->description == NULL)
+		return PMINFO_R_ERROR;
+
 	for (tmp = info->pkg_info->description; tmp; tmp = tmp->next) {
 		ptr = (description_x *)tmp->data;
-		if (ptr && ptr->text == NULL)
-			ptr->text = strdup("");
 		if (ptr == NULL || ptr->text == NULL || ptr->lang == NULL ||
 				strcmp(ptr->lang, locale))
 			continue;
@@ -1277,8 +1277,6 @@ API int pkgmgrinfo_pkginfo_get_description(pkgmgrinfo_pkginfo_h handle, char **d
 	locale = DEFAULT_LOCALE;
 	for (tmp = info->pkg_info->description; tmp; tmp = tmp->next) {
 		ptr = (description_x *)tmp->data;
-		if (ptr && ptr->text == NULL)
-			ptr->text = strdup("");
 		if (ptr == NULL || ptr->text == NULL || ptr->lang == NULL ||
 				strcmp(ptr->lang, locale))
 			continue;
@@ -1286,7 +1284,9 @@ API int pkgmgrinfo_pkginfo_get_description(pkgmgrinfo_pkginfo_h handle, char **d
 		return PMINFO_R_OK;
 	}
 
-	return PMINFO_R_ERROR;
+	*description = "";
+
+	return PMINFO_R_OK;
 }
 
 API int pkgmgrinfo_pkginfo_get_author_name(pkgmgrinfo_pkginfo_h handle, char **author_name)
